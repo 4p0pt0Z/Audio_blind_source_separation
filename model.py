dfdc17887f27e7dbe2b23f7fd7ff9f41b0d06a13
Vincent <vincent.pollet@epfl.ch>
2018-09-24 16:54:29 +0200

    Adds global weighted rank pooling, channel wise fully connected layer, and offer both these options in the VGG-like model

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as func
 
 
 def find_model_class(model_type):
@@ -16,6 +15,35 @@ def find_model_class(model_type):
         return VGG_like_CNN
     else:
         raise NotImplementedError("The model type " + model_type + " is not available.")
+
+
+class ChannelWiseFC(nn.Module):
+    def __init__(self, in_channels, in_features, out_features, bias=True):
+        super(ChannelWiseFC, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.in_channels = in_channels
+        self.FCs = nn.ModuleList([nn.Linear(in_features, out_features, bias) for _ in range(in_channels)])
+
+    def forward(self, x):
+        y = torch.zeros((x.shape[0], self.in_channels, self.out_features), device=x.device)
+        for channel in range(self.in_channels):
+            y[:, channel, :] = self.FCs[channel](x[:, channel, :])
+        return y
+
+
+class GlobalWeightedRankPooling(nn.Module):
+    def __init__(self, dc, size):
+        super(GlobalWeightedRankPooling, self).__init__()
+        self.register_buffer('dcs', torch.Tensor([dc ** j for j in range(size)]))
+        self.register_buffer('Z', torch.sum(self.dcs))
+
+    def forward(self, x):
+        x, _ = torch.sort(x, dim=2, descending=True)
+        y = torch.zeros((x.shape[0], x.shape[1]), device=x.device)
+        for c in range(x.shape[1]):
+            y[:, c] = torch.sum(x[:, c, :] * self.dcs, dim=1) / self.Z
+        return y
 
 
 class VGG_like_CNN(nn.Module):
@@ -37,9 +65,12 @@ class VGG_like_CNN(nn.Module):
             "groups": [1, 1, 1, 1, 1, 1],  # groups parameter for each conv layer.
             "drop_out_probs": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # drop out probability during training for each bloc
 
-            "use_batch_norm": True,  # If true, each bloc contains a batch normalization layer after the convolution
+            "leaky_relu_slope": 0.01,
 
-            "classification_mapping": "GAP"  # The classification mapping to use: {"GMP", "GAP", "GWRP"}
+            "use_batch_norm": True,  # If true, batch normalization is used between the convolutions and activations
+
+            "classification_mapping": "GAP",  # The classification mapping to use: {"GMP", "GAP", "GWRP"}
+            "dc": 0.5  # hyper-parameter for global weighted pooling
         }
         return config
 
@@ -53,29 +84,40 @@ class VGG_like_CNN(nn.Module):
                                                      (config["p_f"][i], config["p_t"][i]))
                                            for i in range(self.n_blocs)])
 
+        for conv in self.convolutions:
+            nn.init.kaiming_normal_(conv.weight, a=config["leaky_relu_slope"], nonlinearity="leaky_relu")
+
         self.use_batch_norm = config["use_batch_norm"]
         if self.use_batch_norm:
             self.batch_norms = nn.ModuleList([nn.BatchNorm2d(config["o_c"][i]) for i in range(self.n_blocs)])
 
-        self.activations = nn.ModuleList([nn.LeakyReLU(0.5) for _ in range(self.n_blocs - 1)] + [nn.Sigmoid()])
+        self.activations = nn.ModuleList([nn.LeakyReLU(config["leaky_relu_slope"])
+                                          for _ in range(self.n_blocs - 1)] + [nn.Sigmoid()])
 
-        self.dropouts = nn.ModuleList([nn.Dropout(config["drop_out_probs"][i]) for i in range(self.n_blocs)])
+        self.dropouts = nn.ModuleList([nn.Dropout2d(config["drop_out_probs"][i]) for i in range(self.n_blocs)])
 
-        # Run a variable through the network to get the shape of the input to the classification mapping
+        # Run a variable through the convolutions to get the shape of the input to the classification mapping
         example_input = torch.zeros((1, input_shape[0], input_shape[1], input_shape[2]))  # add batch dimension
         for idx in range(self.n_blocs):
             example_input = self.convolutions[idx](example_input)
-            if self.use_batch_norm:
-                example_input = self.batch_norms[idx](example_input)
-            example_input = self.dropouts[idx](example_input)
             example_input = self.activations[idx](example_input)
 
+        self.mask_shape = (example_input.shape[-2], example_input.shape[-1])
+
         if config["classification_mapping"] == "GMP":
-            self.c_map = nn.MaxPool2d((example_input.shape[-2], example_input.shape[-1]))
+            self.c_map = nn.MaxPool2d(self.mask_shape)
+            self.reshape = False
         elif config["classification_mapping"] == "GAP":
-            self.c_map = nn.AvgPool2d((example_input.shape[-2], example_input.shape[-1]))
+            self.c_map = nn.AvgPool2d(self.mask_shape)
+            self.reshape = False
         elif config["classification_mapping"] == "GWRP":
-            raise NotImplementedError("TODO !")
+            self.c_map = GlobalWeightedRankPooling(config["dc"], example_input.shape[2] * example_input.shape[3])
+            self.reshape = True
+        elif config["classification_mapping"] == "FC":
+            self.c_map = nn.Sequential(ChannelWiseFC(example_input.shape[1],
+                                                     example_input.shape[2] * example_input.shape[3], 1),
+                                       nn.Sigmoid())
+            self.reshape = True
 
     def forward(self, x):
         for idx in range(self.n_blocs):
@@ -86,5 +128,7 @@ class VGG_like_CNN(nn.Module):
             x = self.activations[idx](x)
 
         masks = x.detach().cpu().numpy()
+        if self.reshape:
+            x = x.view(x.shape[0], x.shape[1], -1)
         x = self.c_map(x).squeeze()  # squeeze to remove reduced dimensions
         return x, masks
